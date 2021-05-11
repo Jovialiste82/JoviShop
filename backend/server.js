55a3efe57c0c4b9d1efc1df80b4d7e31cb3bfd6a
Jovialiste82 <52076767+Jovialiste82@users.noreply.github.com>
2021-05-10 21:44:30 -0400

    User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 // allows to use environment variables stored in .env
 dotenv.config();
@@ -25,11 +26,16 @@ const app = express();
 //   next();
 // });
 
+// Middleware allowing req.body to be parsed
+// allows us to accept JSON data in the body
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
