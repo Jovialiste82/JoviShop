68947576ea28dd7dff40e6dd05b9ff00609c9df0
Jovialiste82 <52076767+Jovialiste82@users.noreply.github.com>
2021-05-13 23:38:45 -0400

    Profile Screen & Get User Details

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -5,6 +5,9 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
   USER_LOGOUT,
 } from "../constants";
 
@@ -17,7 +20,7 @@ export const userLoginReducer = (state = {}, action) => {
     case USER_LOGIN_FAIL:
       return { loading: false, error: action.payload };
     case USER_LOGOUT:
-      return {};
+      return { userInfo: null };
     default:
       return state;
   }
@@ -30,6 +33,19 @@ export const userRegisterReducer = (state = {}, action) => {
     case USER_REGISTER_SUCCESS:
       return { loading: false, userInfo: action.payload };
     case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case USER_DETAILS_SUCCESS:
+      return { loading: false, user: action.payload };
+    case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
