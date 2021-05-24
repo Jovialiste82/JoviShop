5eb1bfeab07a980420657dc773f1a6f342091894
Jovialiste82 <52076767+Jovialiste82@users.noreply.github.com>
2021-05-24 01:54:15 -0400

    Admin Middleware & Get Users Endpoint

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -27,4 +27,13 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-export { protect };
+const admin = asyncHandler(async (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Error (0462): Not authorized as an Admin");
+  }
+});
+
+export { protect, admin };
