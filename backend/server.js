d6afc556f7c8092b94ea3764bdd4643306764c9d
Jovialiste82 <52076767+Jovialiste82@users.noreply.github.com>
2021-05-27 13:54:54 -0400

    Morgan & Create Review Endpoint

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import express from "express";
 import path from "path";
 import dotenv from "dotenv";
 import colors from "colors";
+import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -28,6 +29,9 @@ const app = express();
 //   console.log(req.originalUrl);
 //   next();
 // });
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Middleware allowing req.body to be parsed
 // allows us to accept JSON data in the body
